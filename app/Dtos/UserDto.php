d254b5f2ab93914663fc3d59308c67f3d201df72
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-04-04 16:19:38 -0700

    DTO updates

--- app/Dtos/UserDto.php
+++ app/Dtos/UserDto.php
@@ -4,14 +4,41 @@ declare(strict_types=1);
 
 namespace App\Dtos;
 
+use App\Enums\UserStateEnum;
+
 readonly class UserDto
 {
     public function __construct(
-        public string $username,
-        public string $password,
-        public string $email,
+        public ?int $id,
         public ?string $name,
+        public string $username,
+        public ?string $birthdate,
+        public bool $birthdate_year_only,
+        public ?string $gender,
+        public ?string $pronouns,
+        public ?string $relationship_status,
+        public string $salt,
+        public string $hashed_password,
+        public string $email,
+        public ?string $email_verified_at,
+        public bool $show_email,
+        public bool $use_mefi_mail,
+        public ?string $paypal_email,
         public ?string $homepage_url,
-        public string $state,
+        public string $ip_address,
+        public ?string $latitude,
+        public ?string $longitude,
+        public ?int $nearby,
+        public ?int $regional,
+        public bool $show_coordinates,
+        public bool $agrees_to_terms,
+        public bool $is_admin,
+        public bool $is_banned,
+        public bool $show_donate,
+        public bool $show_share_links,
+        public string $created_at,
+        public ?string $updated_at,
+        public ?string $deleted_at,
+        public UserStateEnum $user_state,
     ) {}
 }
