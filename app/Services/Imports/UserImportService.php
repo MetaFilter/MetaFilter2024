c06fc7ea931aec859e4358fc662296174848efec
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-04-04 16:18:49 -0700

    Service updates

--- app/Services/Imports/UserImportService.php
+++ app/Services/Imports/UserImportService.php
@@ -4,37 +4,48 @@ declare(strict_types=1);
 
 namespace App\Services\Imports;
 
-use App\Dtos\ImportDto;
-use App\Models\User;
+use App\Jobs\Import\ImportUser;
 use App\Traits\ImportTrait;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\LazyCollection;
+use RuntimeException;
 
 final class UserImportService
 {
     use ImportTrait;
 
-    public const array COLUMNS = [
-        'first_name',
-        'last_name',
-        'email',
-        'phone',
-        'street_address',
-        'city',
-        'state',
-        'postal_code',
-        'country',
-        'date_of_birth',
-    ];
+    private int $counter;
+
+    public function __construct()
+    {
+        $this->counter = 0;
+    }
 
     public function import(): void
     {
-        $dto = new ImportDto(
-            model: new User(),
-            filePath: 'app/test,csv',
-            columns: self::COLUMNS,
-            columnLabel: 'date_of_birth',
-            columnValue: "STR_TO_DATE(@date_of_birth, '%c/%d/%Y')"
-        );
+        $this->prepareDatabaseForImport();
 
-        $this->importData($dto);
+        DB::table('users')->truncate();
+
+        LazyCollection::make(items: function () {
+            $file = fopen(storage_path('app/imports/users_all.csv'), mode: 'r');
+
+            if ($file === false) {
+                throw new RuntimeException(message: 'Failed to open the file.');
+            }
+
+            while (($row = fgets($file)) !== false) {
+                yield str_getcsv($row);
+            }
+        })
+        ->each(function ($row) {
+            $this->counter++;
+
+            if ($this->counter === 2) {
+                \Log::debug('UserImportService row' . print_r($row, true));
+                ImportUser::dispatch($row);
+                //                dispatch(new ImportUser($row));
+            }
+        });
     }
 }
