55bd9a7037e55beca4a200bee8e0239c18253b09
Anthony Plunkett <anthony@thefort.org>
2025-05-27 16:09:37 -0400

    show joined date based on database (was static)

--- resources/views/members/partials/info.blade.php
+++ resources/views/members/partials/info.blade.php
@@ -1,13 +1,10 @@
 <section>
     <h2>{{ trans('Info') }}</h2>
 
-    <table class="profile">
-        <tbody>
-            <tr>
-                <th scope="row">Joined:</th>
-                {{-- TODO: Link to archive page --}}
-                <td class="nowrap">June 18, 2017</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="profile">
+        <dl>
+            <dt>Joined</dt>
+            <dd><x-dates.formatted-date-time-component :date="$user->created_at" format="F j, Y" /></dl>
+        </dl>
+    </div>
 </section>
