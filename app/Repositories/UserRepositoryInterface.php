374afbcdf388de4717e873917aebeea1504926bd
Anthony Plunkett <anthony@thefort.org>
2025-05-27 16:12:42 -0400

    add query to find stale users
    could move the current 24hr window to a .env variable.

--- app/Repositories/UserRepositoryInterface.php
+++ app/Repositories/UserRepositoryInterface.php
@@ -6,10 +6,14 @@ namespace App\Repositories;
 
 use App\Dtos\UserDto;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
 
 interface UserRepositoryInterface extends BaseRepositoryInterface
 {
     public function createUser(UserDto $dto);
 
     public function updateState(User $user, string $state);
+
+    public function findStaleUsers(): Collection;
 }
